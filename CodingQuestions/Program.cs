5b2953f9f2830ccf0c3bcf5a5048a2f96cea05ad
Arslan Durrani <arslandurrani@outlook.com>
2024-05-15 15:34:46 +0500

    Add project files.

---
+++ CodingQuestions/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace OOPConcepts
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            How to find all possible substring of a given string?
+            input: abcd
+            output : a ab abc abcd b bc bcd c cd d
+             */
+            Questions.substring("abcd");
+
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ CodingQuestions/Questions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPConcepts
+{
+    internal static class Questions
+    {
+        public static void substring(string str)
+        {
+            for (int start = 0; start < str.Length; start++)
+            {
+                for (int end = start+1; end <= str.Length; end++) 
+                {
+                    var substr = str.Substring(start, end-start);
+                    Console.WriteLine(substr);
+                }
+            }
+        }
+    }
+}
